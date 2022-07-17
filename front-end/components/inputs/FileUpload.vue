00b5d45a13041801557d2c7f5a5115ea7ac1aa61
Mabrur Syamhur <56716500+imboyo@users.noreply.github.com>
2022-07-17 20:18:34 +0800

    fileupload form validation

--- front-end/components/inputs/FileUpload.vue
+++ front-end/components/inputs/FileUpload.vue
@@ -1,14 +1,36 @@
 <script lang="ts" setup>
+import { validateField } from "~/my_modules/input_validation";
+import MyErrorMessage from "~/components/inputs/MyErrorMessage.vue";
+
+// Ref templates
 const file = ref(null);
+
+// State
+const errorState = ref<boolean | string>(false);
+
+const inputRules = [
+  {
+    text: "This field is required",
+    validate: () => file.value.files.length > 0,
+  },
+];
+
+// Pass this function to parent as ref
+const refreshValidation = () => {
+  errorState.value = validateField(file.value.files.length, inputRules);
+};
 </script>
 
 <template>
-  <input
-    class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
-    id="file_input"
-    type="file"
-    ref="file"
-    @change="$emit('fileChanged', file.files[0])"
-    required
-  />
+  <div class="flex flex-col gap-1">
+    <input
+      class="block w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 cursor-pointer dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+      :class="[errorState ? 'border-error-300' : '']"
+      id="file_input"
+      type="file"
+      ref="file"
+      @change="[$emit('fileChanged', file.files[0]), refreshValidation()]"
+    />
+    <MyErrorMessage v-if="errorState" :errorState="errorState" />
+  </div>
 </template>
