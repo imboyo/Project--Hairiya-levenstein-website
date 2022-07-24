837abf77e4321b5cd082b2dcf25e33a31ff981d6
Mabrur Syamhur <56716500+imboyo@users.noreply.github.com>
2022-07-25 00:14:54 +0800

    final setup django

--- back-end/plagiarism_checker/settings.py
+++ back-end/plagiarism_checker/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
     'django_filters',
     'rest_framework',
     'rest_framework_simplejwt',
+    'djoser',
 
     'proposal',
     'user'
@@ -82,7 +83,7 @@ WSGI_APPLICATION = 'plagiarism_checker.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'plagiarism_checker',
+        'NAME': 'plagiarism-checker',
         'USER': 'root',
         'PASSWORD': 'ayyu230686',
         'HOST': 'localhost',  # Or an IP Address that your DB is hosted on
@@ -123,6 +124,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
