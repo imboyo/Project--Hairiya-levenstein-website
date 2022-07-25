6f7ba31374598f3c6650f348f80dd08b5f6783e1
Mabrur Syamhur <56716500+imboyo@users.noreply.github.com>
2022-07-25 09:22:44 +0800

    proposal models

--- back-end/proposal/models.py
+++ back-end/proposal/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
+
 # Create your models here.
+class Proposal(models.Model):
+    mahasiswa = models.ManyToManyField('auth.User', related_name='mahasiswa')
+    dosen = models.ManyToManyField('auth.User', related_name='dosen')
+    title = models.CharField(max_length=100)
+    file = models.FileField(upload_to='proposal/')
+    created_at = models.DateTimeField(auto_now_add=True)
+    plagiarism_percentage = models.FloatField(default=0)
+
+    def __str__(self):
+        return self.title
