b02508230ab9687e2fe195fd43dc625b4f35b90b
Mabrur Syamhur <56716500+imboyo@users.noreply.github.com>
2022-07-26 01:30:27 +0800

    fixing layout bug

--- front-end/pages/auth/login.vue
+++ front-end/pages/auth/login.vue
@@ -12,22 +12,19 @@ import { verifyLogin } from "~/my_modules/auth";
 import { baseApiUrl } from "~/my_modules/environment";
 
 // Page Meta and main data
-definePageMeta({
-  layout: "auth",
-});
 useHead({
   titleTemplate: (title) => `Login - ${title}`,
 });
 
 const router = useRouter();
 // Check Logged in or not
-onMounted(() =>
+onMounted(() => {
   verifyLogin(
     () => router.push({ path: "/admin/dashboard" }),
     () => {},
     () => {}
-  )
-);
+  );
+});
 
 // * State
 // Form Input Rules
@@ -158,90 +155,92 @@ const handleClick = async () => {
 </script>
 
 <template>
-  <div
-    class="flex flex-col h-screen justify-center gap-8 w-full md:w-[22.5rem]"
-  >
-    <!-- * Content Container  -->
-    <!-- Header  -->
-    <div class="flex flex-col items-center justify-center gap-6">
-      <!--   Image wrapper   -->
-      <!--    TODO : Perbaiki nanti logo imagenya    -->
-      <ImageWrapper
-        rounded="2xl"
-        img="/img/64ff580d4559faeaf7fc7e1ffd13cb5a.jpg"
-        class="w-12 h-12"
-      />
-      <!--   ! End Image wrapper    -->
-      <!--   Text Header   -->
-      <AuthTextHeader>
-        <template #title>Silahkan login</template>
-        <template #subTitle>Selamat datang kembali!</template>
-      </AuthTextHeader>
-      <!--  !  End Text Header    -->
-    </div>
-    <!-- ! End Header  -->
-    <!--   Form   -->
-    <div class="flex flex-col gap-6">
-      <!--   Input Field   -->
-      <div class="flex flex-col gap-5">
-        <InputField
-          placeholder="Email"
-          :rules="formInputRules.username"
-          type="text"
-          label="Username"
-          required
-          ref="usernameField"
-          @typing="
-            formValues.username = $event.inputValue;
-            formErrorValues.username = $event.errorState;
-          "
+  <NuxtLayout name="auth">
+    <div
+      class="flex flex-col h-screen justify-center gap-8 w-full md:w-[22.5rem]"
+    >
+      <!-- * Content Container  -->
+      <!-- Header  -->
+      <div class="flex flex-col items-center justify-center gap-6">
+        <!--   Image wrapper   -->
+        <!--    TODO : Perbaiki nanti logo imagenya    -->
+        <ImageWrapper
+          rounded="2xl"
+          img="/img/64ff580d4559faeaf7fc7e1ffd13cb5a.jpg"
+          class="w-12 h-12"
         />
-        <InputField
-          placeholder="Password"
-          :rules="formInputRules.password"
-          type="email"
-          label="Password"
-          required
-          ref="passwordField"
-          @typing="
-            formValues.password = $event.inputValue;
-            formErrorValues.password = $event.errorState;
-          "
-        />
+        <!--   ! End Image wrapper    -->
+        <!--   Text Header   -->
+        <AuthTextHeader>
+          <template #title>Silahkan login</template>
+          <template #subTitle>Selamat datang kembali!</template>
+        </AuthTextHeader>
+        <!--  !  End Text Header    -->
       </div>
-      <!--  ! End Input Field   -->
-      <!--   Button   -->
+      <!-- ! End Header  -->
+      <!--   Form   -->
       <div class="flex flex-col gap-6">
-        <div class="flex flex-row">
-          <div class="flex w-6/12 justify-start">
-            <InputCheckBox
-              id="remember_me"
-              @change="formValues.checked = $event.target.value"
-              >Ingat Saya
-            </InputCheckBox>
-          </div>
-          <!--    TODO: Linknya nanti perbaiki      -->
-          <div class="flex w-6/12 justify-end">
-            <NuxtLink
-              to="forgot-password"
-              class="text-primary-700 text-sm font-medium inline-block"
-            >
-              Lupa Password
-            </NuxtLink>
-          </div>
+        <!--   Input Field   -->
+        <div class="flex flex-col gap-5">
+          <InputField
+            placeholder="Email"
+            :rules="formInputRules.username"
+            type="text"
+            label="Username"
+            required
+            ref="usernameField"
+            @typing="
+              formValues.username = $event.inputValue;
+              formErrorValues.username = $event.errorState;
+            "
+          />
+          <InputField
+            placeholder="Password"
+            :rules="formInputRules.password"
+            type="email"
+            label="Password"
+            required
+            ref="passwordField"
+            @typing="
+              formValues.password = $event.inputValue;
+              formErrorValues.password = $event.errorState;
+            "
+          />
         </div>
-        <MyButton
-          hieararchy="primary"
-          size="lg"
-          width="full"
-          :disabled="formIsError || buttonIsLoading"
-          @clicked="handleClick"
-        >
-          <template #text>Login</template>
-        </MyButton>
+        <!--  ! End Input Field   -->
+        <!--   Button   -->
+        <div class="flex flex-col gap-6">
+          <div class="flex flex-row">
+            <div class="flex w-6/12 justify-start">
+              <InputCheckBox
+                id="remember_me"
+                @change="formValues.checked = $event.target.value"
+                >Ingat Saya
+              </InputCheckBox>
+            </div>
+            <!--    TODO: Linknya nanti perbaiki      -->
+            <div class="flex w-6/12 justify-end">
+              <NuxtLink
+                to="forgot-password"
+                class="text-primary-700 text-sm font-medium inline-block"
+              >
+                Lupa Password
+              </NuxtLink>
+            </div>
+          </div>
+          <MyButton
+            hieararchy="primary"
+            size="lg"
+            width="full"
+            :disabled="formIsError || buttonIsLoading"
+            @clicked="handleClick"
+          >
+            <template #text>Login</template>
+          </MyButton>
+        </div>
+        <!--  ! End Button   -->
       </div>
-      <!--  ! End Button   -->
+      <!--   ! End Container   -->
     </div>
-    <!--   ! End Container   -->
-  </div>
+  </NuxtLayout>
 </template>
