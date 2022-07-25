ec379d3c71c9043a80c66c00a26b5b199f211eb8
Mabrur Syamhur <56716500+imboyo@users.noreply.github.com>
2022-07-25 14:54:36 +0800

    fix bug djoser

--- back-end/plagiarism_checker/settings.py
+++ back-end/plagiarism_checker/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os
+import datetime
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -150,4 +151,10 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000"
 ]
 
-
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(days=7),
+    'REFRESH_TOKEN_LIFETIME': datetime.timedelta(days=7),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': False,
+    'UPDATE_LAST_LOGIN': False,
+}
